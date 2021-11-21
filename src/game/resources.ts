169bb616a11052bb954534d67642359e849952e5
Dlabarts <hubert4j@gmail.com>
2021-11-21 22:38:32 +0100

    updates hero Exias ruuning

--- src/game/resources.ts
+++ src/game/resources.ts
@@ -5,6 +5,7 @@ export const images: { [key: string]: ImageSource } = {
     hero_idle: new ImageSource(require("../resources/hero_idle.png").default),
     heroExias_idle: new ImageSource(require("../resources/Exias_idleV4.png").default),
     hero_run: new ImageSource(require("../resources/hero_run.png").default),
+   // heroExias_run: new ImageSource(require("../resources/Exias_runningV2.png")),
     hero_jump: new ImageSource(require("../resources/hero_jump.png").default),
     hero_down: new ImageSource(require("../resources/hero_down.png").default),
     hero_attack: new ImageSource(require("../resources/hero_attack.png").default),
@@ -14,9 +15,7 @@ export const images: { [key: string]: ImageSource } = {
     fox: new ImageSource(require("../resources/fox.png").default)
 }
 
-export const sounds = {
-
-}
+export const sounds = {}
 
 const default_hero_grid = {
     columns: 1,
@@ -86,6 +85,11 @@ export const hero_run_sheet = SpriteSheet.fromImageSource({
     image: images.hero_run,
     grid: default_hero_grid
 });
+
+/*export const heroExias_run_sheet = SpriteSheet.fromImageSource({
+    image: images.heroExias_run,
+    grid: {...default_grid, rows: 9}
+})*/
 
 export const hero_jump_sheet = SpriteSheet.fromImageSource({
     image: images.hero_jump,
